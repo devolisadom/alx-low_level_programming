1f47564f517706a47e91e9d738fcc27ebba1bf67
Olisa Dominic <cheerfuloli@hotmail.com>
2022-10-19 02:02:19 +0100

    functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,29 +1,51 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_to_98 - prints all the natural numbers from n to 98
- *
- * @n: interger to be tested
- * Return: list of numbers
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
  */
-void print_to_98(int n)
-{
-	int i;
 
-	if (n <= 98)
+void print_times_table(int n)
+{
+
+int a = 0, rep, b;
+
+if (n < 0 || n > 15)
+	return;
+
+while (a <= n)
+{
+	for (b = 0; b <= n; b++)
 	{
-		for (i = n; i < 98; i++)
+		rep = a * b;
+		if (b == 0)
+			_putchar('0' + rep);
+		else if (rep < 10)
 		{
-			printf("%i, ", i);
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + rep);
+		}
+		else if (rep < 100)
+		{
+			_putchar(' ');
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+		else
+		{
+			_putchar('0' + rep / 100);
+			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + rep % 10);
+		}
+		if (b < n)
+		{
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
-	else
-	{
-		for (i = n; i > 98; i--)
-		{
-			printf("%i, ", i);
-		}
-	}
-	printf("98\n");
+	_putchar('\n');
+	a++;
+}
 }
